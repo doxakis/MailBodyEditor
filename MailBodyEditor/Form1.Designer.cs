72348e55a3c432ce88299bdf8fe29c9bfe130f34
Philip Doxakis <philip@doxakis.com>
2017-07-08 15:54:41 -0400

    POC: version 1

--- MailBodyEditor/Form1.Designer.cs
+++ MailBodyEditor/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.previewBox = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCode.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCode.Location = new System.Drawing.Point(3, 3);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(394, 595);
+            this.textBoxCode.TabIndex = 0;
+            this.textBoxCode.TextChanged += new System.EventHandler(this.textBoxCode_TextChanged);
+            // 
+            // previewBox
+            // 
+            this.previewBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewBox.Location = new System.Drawing.Point(3, 3);
+            this.previewBox.MinimumSize = new System.Drawing.Size(20, 20);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(489, 595);
+            this.previewBox.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxCode);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.previewBox);
+            this.splitContainer1.Size = new System.Drawing.Size(899, 601);
+            this.splitContainer1.SplitterDistance = 400;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(899, 601);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "MailBody Editor";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.WebBrowser previewBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- MailBodyEditor/Form1.cs
+++ MailBodyEditor/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,86 @@ namespace MailBodyEditor
         public Form1()
         {
             InitializeComponent();
+
+            textBoxCode.Text += "var body = MailBody" + Environment.NewLine +
+                "    .CreateBody()" + Environment.NewLine +
+                "    .Paragraph(\"Please confirm your email address by clicking the link below.\")" + Environment.NewLine +
+                "    .Paragraph(\"We may need to send you critical information about our service and it is important that we have an accurate email address.\")" + Environment.NewLine +
+                "    .Button(\"https:/" + " / example.com/\", \"Confirm Email Address\")" + Environment.NewLine +
+                "    .Paragraph(\"— [Insert company name here]\")" + Environment.NewLine +
+                "    .ToString(); " + Environment.NewLine;
+            textBoxCode.SelectionStart = textBoxCode.TextLength;
+            textBoxCode.ScrollToCaret();
+        }
+
+        private void textBoxCode_TextChanged(object sender, EventArgs e)
+        {
+            var results = CompileCsharpCode(textBoxCode.Text);
+
+            StringBuilder builder = new StringBuilder();
+
+            if (results.Errors.HasErrors)
+            {
+                foreach (CompilerError item in results.Errors)
+                {
+                    builder.Append(!item.IsWarning ? "<span style='color:red'>" : "<span style='color:orange'>")
+                        .Append(item.ErrorText)
+                        .Append("</span>")
+                        .Append("<br />");
+                }
+            }
+            else
+            {
+                builder.Append(RunCSharpCode(results));
+            }
+
+            previewBox.DocumentText = "<html><body>" +
+                builder.ToString() +
+                "</body></html>";
+        }
+
+        public static CompilerResults CompileCsharpCode(string function)
+        {
+            string code = @"
+                using System;
+                using MailBodyPack;
+                namespace UserFunctions
+                {
+                    public class MyFunction
+                    {
+                        public static string Function()
+                        {
+                            myfunc;
+                            return body;
+                        }
+                    }
+                }
+            ";
+            
+            string finalCode = code.Replace("myfunc", function);
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters compilerParameters = new CompilerParameters();
+            compilerParameters.GenerateInMemory = true;
+            compilerParameters.ReferencedAssemblies.Add("System.dll");
+            compilerParameters.ReferencedAssemblies.Add("System.Linq.dll");
+            compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
+            compilerParameters.ReferencedAssemblies.Add("MailBody.dll");
+            CompilerResults results = provider.CompileAssemblyFromSource(compilerParameters, finalCode);
+            return results;
+        }
+        
+        public static string RunCSharpCode(CompilerResults results)
+        {
+            Type myType = results.CompiledAssembly.GetType("UserFunctions.MyFunction");
+            try
+            {
+                var result = myType.GetMethod("Function").Invoke(null, new object[] { }).ToString();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
